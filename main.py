7c429c734940dd321e5233a27e66c9e121d59337
SachidanandSharma2162 <sharmasachidanand1111@gmail.com>
2026-07-03 11:33:01 +0530

    registered summary tool

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app.tools.add_expense import register_add_expense_tool
 from app.tools.list_expense import register_list_expense_tool
 from app.tools.edit_expense import register_edit_expense_tool
 from app.tools.delete_expense import register_delete_expense_tool
+from app.tools.summarize import register_summary_tool
 
 mcp = FastMCP("Expense Tracker",)
 
@@ -11,5 +12,6 @@ register_add_expense_tool(mcp)
 register_list_expense_tool(mcp)
 register_edit_expense_tool(mcp)
 register_delete_expense_tool(mcp)
+register_summary_tool(mcp)
 if __name__ == "__main__":
     mcp.run()
